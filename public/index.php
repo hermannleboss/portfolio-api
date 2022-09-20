827313371a92065570fec5141fbc792a1f3d4336
Hermann EHO <hermannleboss@gmail.com>
2022-09-20 19:07:49 +0000

    trusted host

--- public/index.php
+++ public/index.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Kernel;
-
+use Symfony\Component\HttpFoundation\Request;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
+Request::setTrustedProxies(
+// the IP address (or range) of your proxy
+    ['192.0.0.1', '10.0.0.0/8'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_FORWARDED
+
+// or, if your proxy instead uses the "Forwarded" header
+// Request::HEADER_FORWARDED
+
+// or, if you're using AWS ELB
+// Request::HEADER_X_FORWARDED_AWS_ELB
+);
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
